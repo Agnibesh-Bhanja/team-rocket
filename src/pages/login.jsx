2e942bf599d6f0b64a52680cec9c71464a13c41a
Agnibesh-Bhanja <agnibeshbhanja@gmail.com>
2026-02-21 18:57:32 +0530

    Auth system and database configured

---
+++ src/pages/login.jsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import { supabase } from "../lib/supabase";
+
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async () => {
+    const { error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+
+    if (error) alert(error.message);
+    else alert("Logged in!");
+  };
+
+  const handleSignup = async () => {
+    const { error } = await supabase.auth.signUp({
+      email,
+      password,
+    });
+
+    if (error) alert(error.message);
+    else alert("Account created!");
+  };
+
+  return (
+    <div style={{ padding: "20px" }}>
+      <h2>Team Rocket Portal</h2>
+
+      <input
+        placeholder="Email"
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <br /><br />
+
+      <input
+        type="password"
+        placeholder="Password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <br /><br />
+
+      <button onClick={handleLogin}>Login</button>
+      <button onClick={handleSignup}>Signup</button>
+    </div>
+  );
+}
